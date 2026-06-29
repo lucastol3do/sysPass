97b05e6241272d28ed5b1ca5342bd7213c95d5c2
Lucas Toledo <lucas@subredes.com.br>
2026-06-29 06:45:34 -0300

    feat(sprint5): enable log_errors in production
    Added ini_set('log_errors', 1) alongside display_errors=0 to ensure
    PHP errors are always logged even when not displayed to users.

--- lib/SP/Bootstrap.php
+++ lib/SP/Bootstrap.php
@@ -468,6 +468,7 @@ final class Bootstrap
             } else {
                 error_reporting(E_ALL & ~(E_DEPRECATED | E_STRICT | E_NOTICE));
                 ini_set('display_errors', 0);
+                ini_set('log_errors', 1);
             }
         }
 
